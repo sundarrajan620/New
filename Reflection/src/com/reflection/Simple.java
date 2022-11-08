dfc0eae331db019f46d7f73d964c619bf755d73b
Sundarajan P <Sundarajan P@LAPTOP-8IGDA3BT>
2022-11-08 14:30:00 +0530

    newone

---
+++ Reflection/src/com/reflection/Simple.java
@@ -0,0 +1,5 @@
+package com.reflection;
+
+public class Simple {
+
+}
---
+++ Reflection/src/com/reflection/SimpleChild.java
@@ -0,0 +1,10 @@
+package com.reflection;
+
+public class SimpleChild {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
